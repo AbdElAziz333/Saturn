12d0374a814849ff0f35a4852a2322836b6a621d
AbdElAziz <89092953+AbdElAziz333@users.noreply.github.com>
2022-09-09 21:57:28 +0200

    first release

---
+++ src/main/java/com/abdelaziz/saturn/Saturn.java
@@ -0,0 +1,10 @@
+package com.abdelaziz.saturn;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("saturn")
+public class Saturn {
+    public static final Logger LOGGER = LogManager.getLogger();
+}
---
+++ src/main/java/com/abdelaziz/saturn/mixin/world/biome/BiomeMixin.java
@@ -0,0 +1,25 @@
+package com.abdelaziz.saturn.mixin.world.biome;
+
+import com.abdelaziz.saturn.common.world.biome.SaturnTemperatureCache;
+import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Supplier;
+
+@Mixin(Biome.class)
+public class BiomeMixin {
+
+    @Redirect(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/ThreadLocal;withInitial(Ljava/util/function/Supplier;)Ljava/lang/ThreadLocal;"
+            )
+    )
+    private ThreadLocal<Long2FloatLinkedOpenHashMap> staticThreadLocal(Supplier<?> supplier) {
+        return SaturnTemperatureCache.saturnTemperatureCache;
+    }
+}
