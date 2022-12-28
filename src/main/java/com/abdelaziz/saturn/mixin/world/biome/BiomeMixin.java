ae6fe227e23ba71a6245a07794d7bc4c0b0e9210
AbdElAziz333 <abdelaziz524aziz@gmail.com>
2022-12-28 12:49:47 +0200

    Improve the biome temperature caching method

--- src/main/java/com/abdelaziz/saturn/mixin/world/biome/BiomeMixin.java
+++ src/main/java/com/abdelaziz/saturn/mixin/world/biome/BiomeMixin.java
@@ -2,6 +2,7 @@ package com.abdelaziz.saturn.mixin.world.biome;
 
 import com.abdelaziz.saturn.common.world.biome.SaturnTemperatureCache;
 import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
+import net.minecraft.Util;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +12,21 @@ import java.util.function.Supplier;
 
 @Mixin(Biome.class)
 public class BiomeMixin {
+
+    private static final ThreadLocal<Long2FloatLinkedOpenHashMap> saturnTemperatureCache = ThreadLocal.withInitial(() ->
+            Util.make(() -> {
+                Long2FloatLinkedOpenHashMap long2FloatLinkedOpenHashMap = new Long2FloatLinkedOpenHashMap(
+                        1024, 0.25F
+                ) {
+                    @Override
+                    protected void rehash(int n) {
+                    }
+                };
+                long2FloatLinkedOpenHashMap.defaultReturnValue(Float.NaN);
+                return long2FloatLinkedOpenHashMap;
+            })
+    );
+
     @Redirect(
             method = "<init>",
             at = @At(
