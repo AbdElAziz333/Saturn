750918f81d77a04a644978160f2ed73087c9f032
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-04-30 09:00:01 +0300

    Change: remove temperature cache size for now.

---
+++ src/main/java/com/abdelaziz/saturn/mixin/world/temperature_cache/BiomeMixin.java
@@ -0,0 +1,44 @@
+package com.abdelaziz.saturn.mixin.world.temperature_cache;
+
+import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Supplier;
+
+@Mixin(Biome.class)
+public abstract class BiomeMixin {
+
+    @Shadow
+    protected abstract float getHeightAdjustedTemperature(BlockPos pos);
+
+    /**
+     * @reason make temperatureCache null, saves the value memory
+     * @author AbdElAziz
+     * */
+    @Redirect(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/ThreadLocal;withInitial(Ljava/util/function/Supplier;)Ljava/lang/ThreadLocal;"
+            )
+    )
+    private ThreadLocal<Long2FloatLinkedOpenHashMap> nullThreadLocal(Supplier<?> supplier) {
+        return null;
+    }
+
+    /**
+     * @reason remove the vanilla temperature
+     * @author AbdElAziz
+     * */
+    @Deprecated
+    @Overwrite
+    public float getTemperature(BlockPos pos) {
+        return getHeightAdjustedTemperature(pos);
+    }
+}
