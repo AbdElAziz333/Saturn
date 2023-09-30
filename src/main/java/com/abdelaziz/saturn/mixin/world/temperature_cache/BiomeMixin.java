4734b5194476ab738c97bc0d98fbdcd0af5e0257
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-09-30 15:59:09 +0300

    set temperatureCache to null

--- src/main/java/com/abdelaziz/saturn/mixin/world/temperature_cache/BiomeMixin.java
+++ src/main/java/com/abdelaziz/saturn/mixin/world/temperature_cache/BiomeMixin.java
@@ -3,35 +3,27 @@ package com.abdelaziz.saturn.mixin.world.temperature_cache;
 import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeGenerationSettings;
-import net.minecraft.world.level.biome.BiomeSpecialEffects;
-import net.minecraft.world.level.biome.MobSpawnSettings;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Supplier;
 
 @Mixin(Biome.class)
 public abstract class BiomeMixin {
     @Shadow
     protected abstract float getHeightAdjustedTemperature(BlockPos pos);
 
-    /**
-     * @reason make temperatureCache null, saves the value memory
-     * @author AbdElAziz
-     * */
-    @Redirect(
+    @Shadow @Final @Mutable
+    private ThreadLocal<Long2FloatLinkedOpenHashMap> temperatureCache;
+
+    @Inject(
             method = "<init>",
             at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/lang/ThreadLocal;withInitial(Ljava/util/function/Supplier;)Ljava/lang/ThreadLocal;"
+                    value = "TAIL"
             )
     )
-    private ThreadLocal<Long2FloatLinkedOpenHashMap> nullThreadLocal(Supplier<?> supplier) {
-        return null;
+    private void nullThreadLocal(CallbackInfo ci) {
+        this.temperatureCache = null;
     }
 
     /**
