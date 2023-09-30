c79715216d64d9d7ad3bc74d2c466229c25cd721
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-09-30 15:58:13 +0300

    Avoid streams to be faster

--- src/main/java/com/abdelaziz/saturn/mixin/ai/brain/clear_memories/BrainMixin.java
+++ src/main/java/com/abdelaziz/saturn/mixin/ai/brain/clear_memories/BrainMixin.java
@@ -1,7 +1,6 @@
 package com.abdelaziz.saturn.mixin.ai.brain.clear_memories;
 
 import com.abdelaziz.saturn.common.ai.brain.ClearableMemory;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.ExpirableValue;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
@@ -13,7 +12,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Mixin(Brain.class)
-public abstract class BrainMixin<E extends LivingEntity> implements ClearableMemory {
+public abstract class BrainMixin implements ClearableMemory {
 
     @Shadow
     @Final
@@ -21,8 +20,8 @@ public abstract class BrainMixin<E extends LivingEntity> implements ClearableMem
 
     @Override
     public void clearMemories() {
-        this.memories.keySet().forEach((p) -> {
-            this.memories.put(p, Optional.empty());
-        });
+        for(MemoryModuleType<?> memoryModuleType : this.memories.keySet()) {
+            this.memories.put(memoryModuleType, Optional.empty());
+        }
     }
 }
