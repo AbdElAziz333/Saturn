dd307658333dad4ebfbb939ecc57d88f728c3dd9
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-10-11 14:28:55 +0300

    Implement all 0.0.5 changes

---
+++ src/main/java/com/abdelaziz/saturn/mixin/world/temperature_cache/BiomeMixin.java
@@ -0,0 +1,38 @@
+package com.abdelaziz.saturn.mixin.world.temperature_cache;
+
+import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Biome.class)
+public abstract class BiomeMixin {
+    @Shadow
+    protected abstract float getHeightAdjustedTemperature(BlockPos pos);
+
+    @Shadow @Final @Mutable
+    private ThreadLocal<Long2FloatLinkedOpenHashMap> temperatureCache;
+
+    @Inject(
+            method = "<init>",
+            at = @At(
+                    value = "TAIL"
+            )
+    )
+    private void nullThreadLocal(CallbackInfo callbackInfo) {
+        temperatureCache = null;
+    }
+
+    /**
+     * @reason remove the vanilla temperature
+     * @author AbdElAziz
+     * */
+    @Deprecated
+    @Overwrite
+    private float getTemperature(BlockPos pos) {
+        return getHeightAdjustedTemperature(pos);
+    }
+}
