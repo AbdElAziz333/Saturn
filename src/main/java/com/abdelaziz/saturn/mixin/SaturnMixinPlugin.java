49c69dd799dbbfed214596b5bb02a1fbbc9b9dbf
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-10-11 14:20:31 +0300

    Implement all 0.0.5 changes

---
+++ src/main/java/com/abdelaziz/saturn/mixin/SaturnMixinPlugin.java
@@ -0,0 +1,58 @@
+package com.abdelaziz.saturn.mixin;
+
+import net.minecraftforge.fml.loading.LoadingModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class SaturnMixinPlugin implements IMixinConfigPlugin {
+    private static final String SATURN_MIXINS_PATH = "com.abdelaziz.saturn.mixin.";
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (LoadingModList.get().getModFileById("memoryleakfix") != null &&
+                mixinClassName.startsWith(SATURN_MIXINS_PATH + "world.temperature_cache")) {
+            return false;
+        }
+
+        if (LoadingModList.get().getModFileById("canary") != null &&
+                mixinClassName.startsWith(SATURN_MIXINS_PATH + "world.threading_detector")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
