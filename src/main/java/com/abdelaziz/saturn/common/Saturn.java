e605238a4081fe6a968e4809ce5b7c2d678ba40b
AbdElAziz333 <AbdElAziz524Aziz@gmail.com>
2023-12-09 05:01:38 +0200

    register the config

--- src/main/java/com/abdelaziz/saturn/common/Saturn.java
+++ src/main/java/com/abdelaziz/saturn/common/Saturn.java
@@ -1,6 +1,10 @@
 package com.abdelaziz.saturn.common;
 
+import com.abdelaziz.saturn.common.config.SaturnConfig;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,4 +12,9 @@ import org.apache.logging.log4j.Logger;
 public class Saturn {
     public static final String MOD_ID = "saturn";
     public static final Logger LOGGER = LogManager.getLogger();
+
+    public Saturn() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SaturnConfig.SPEC);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 }
