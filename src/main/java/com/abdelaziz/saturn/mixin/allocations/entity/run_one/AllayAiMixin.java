3dad566a5d6431eea8a93038c8e353008be7246a
AbdElAziz333 <AbdElAziz524Aziz@gmail.com>
2023-12-24 01:49:43 +0200

    Fix entity ticking issues

--- src/main/java/com/abdelaziz/saturn/mixin/allocations/entity/run_one/AllayAiMixin.java
+++ src/main/java/com/abdelaziz/saturn/mixin/allocations/entity/run_one/AllayAiMixin.java
@@ -1,7 +1,8 @@
 package com.abdelaziz.saturn.mixin.allocations.entity.run_one;
 
 import com.abdelaziz.saturn.common.util.constants.EntityConstants;
-import net.minecraft.world.entity.ai.behavior.DoNothing;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.behavior.RunOne;
 import net.minecraft.world.entity.animal.allay.AllayAi;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +16,10 @@ public class AllayAiMixin {
             method = "initIdleActivity",
             at = @At(
                     value = "NEW",
-                    target = "(II)Lnet/minecraft/world/entity/ai/behavior/DoNothing;"
+                    target = "(Ljava/util/List;)Lnet/minecraft/world/entity/ai/behavior/RunOne;"
             )
     )
-    private static DoNothing useStaticFinalReference(int p_22840_, int p_22841_) {
-        return EntityConstants.DO_NOTHING;
+    private static RunOne<PathfinderMob> useStaticFinalReference(List<?> list) {
+        return EntityConstants.ALLAY;
     }
 }
