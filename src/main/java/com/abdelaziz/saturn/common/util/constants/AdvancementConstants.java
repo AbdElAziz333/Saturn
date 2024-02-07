51b354b254d8d89fbb4f577103573ebf5a8094b0
AbdElAziz333 <AbdElAziz524Aziz@gmail.com>
2024-02-07 20:03:43 +0200

    New: cache ResourceLocation[0] array creation.

---
+++ src/main/java/com/abdelaziz/saturn/common/util/constants/AdvancementConstants.java
@@ -0,0 +1,7 @@
+package com.abdelaziz.saturn.common.util.constants;
+
+import net.minecraft.resources.ResourceLocation;
+
+public interface AdvancementConstants {
+    ResourceLocation[] CACHED_RESOURCE_LOCATION = new ResourceLocation[0];
+}
